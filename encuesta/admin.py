d7f9d6d4b615f8a0e9323efd23d51a9ecad9b8cc
vanedaza <vizes64@hotmail.com>
2020-10-29 16:16:58 -0300

    el proyecto se traslado a otra cuenta

--- encuesta/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Choice, Images
-# Register your models here.
-
-admin.site.register(Images)
-admin.site.register(Choice)
--- encuesta/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EncuestaConfig(AppConfig):
-    name = 'encuesta'
